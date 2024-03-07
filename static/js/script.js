73c2c5f4173def9e3bb8660dda9fcbbfa4f872e3
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-03-07 14:10:37 +0100

    version_2

--- static/js/script.js
+++ static/js/script.js
@@ -71,7 +71,7 @@ function addListItem(
     classes="form-control bg-dark formatuj-maly-font",
     idName="additionalList",
     rowsAmount="4",
-    styleColor="#6d6d6d",
+    styleColor="#c2c2c2",
     styleBorders="#6a6a6a solid 1px",
     req=true,
     itemStyles="no-border formatuj-margin form-control bg-dark formatuj-right",
