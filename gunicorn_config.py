dc9c99663fff1e4f427f99cf6591becc5477ec18
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-03-11 08:20:01 +0100

    version_2

---
+++ gunicorn_config.py
@@ -0,0 +1,12 @@
+import multiprocessing
+
+# Podstawowe konfiguracje
+bind = "0.0.0.0:8000"  # Adres IP i port na którym Gunicorn będzie nasłuchiwał
+workers = multiprocessing.cpu_count() * 2 + 1  # Zalecana liczba procesów pracowniczych
+accesslog = "-"  # Logowanie dostępu; "-" oznacza stdout
+errorlog = "-"  # Logowanie błędów; "-" oznacza stderr
+worker_class = 'sync'  # Typ klasy pracownika (np. sync, gevent, uvicorn.workers.UvicornWorker)
+
+# Zaawansowane konfiguracje
+timeout = 30  # Maksymalny czas oczekiwania na odpowiedź pracownika (w sekundach)
+keepalive = 2  # Czas, po którym połączenie jest zamykane, jeśli nie było aktywności (w sekundach)
