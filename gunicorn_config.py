eefc5fcd575cf45e0eaf7be66d06ebde48d9e207
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-03-11 08:37:57 +0100

    version_2

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,7 +5,7 @@ bind = "185.201.114.123:8000"  # Adres IP i port na którym Gunicorn będzie nas
 workers = multiprocessing.cpu_count() * 2 + 1  # Zalecana liczba procesów pracowniczych
 accesslog = "-"  # Logowanie dostępu; "-" oznacza stdout
 errorlog = "-"  # Logowanie błędów; "-" oznacza stderr
-worker_class = 'sync'  # Typ klasy pracownika (np. sync, gevent, uvicorn.workers.UvicornWorker)
+worker_class = 'gevent'  # Typ klasy pracownika (np. sync, gevent, uvicorn.workers.UvicornWorker)
 
 # Zaawansowane konfiguracje
 timeout = 30  # Maksymalny czas oczekiwania na odpowiedź pracownika (w sekundach)
