2a9642da2672f3712b3ac8a7337aae2b0fd7c5be
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-04-30 12:20:45 +0200

    Working now

--- static/js/script.js
+++ static/js/script.js
@@ -273,7 +273,7 @@ function prepareAndSubmitRentOfferForm(offerId, oldFotos=true) {
     var umeblowanie = document.getElementById('Umeblowanie_' + offerId).value;
     var kuchnia = document.getElementById('FormaKuchni_' + offerId).value;
     var dodatkoweInfo = document.getElementById('InformacjeDodatkowe_' + offerId).value;
-    var offerIDbox = document.getElementById('OfferId_' + offerId).value;
+    var offerIDbox = document.getElementById('OfferID_' + offerId).value;
 
     // Pobieranie zdjęć z listy
     var fotoList = document.getElementById(offerId + '-fileList');
