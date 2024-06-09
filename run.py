88d3440d8985db9a792164b9b3c2c32c52430895
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-06-09 17:44:39 +0200

    Working now

--- run.py
+++ run.py
@@ -4424,6 +4424,10 @@ def public_on_lento():
                         wyswietlanie_na_stronie_glownej_14_dni, wyswietlanie_na_stronie_glownej_30_dni,
                         super_oferta_7_dni, super_oferta_14_dni,
                         4)
+            print('zapytanie')
+            print(zapytanie_sql)
+            print('dane')
+            print(dane)
 
             if msq.insert_to_database(zapytanie_sql, dane):
                 flash(f'Oferta wynajmu została zapisana pomyślnie!', 'success')
