e9d77411125d175e120fa7700966189b38ea211f
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-06-24 12:41:13 +0200

    Working now

--- run.py
+++ run.py
@@ -5817,8 +5817,8 @@ def public_on_facebook():
 
             if 'promuj_po_opublikowaniu' in request.form: promuj_po_opublikowaniu = 1
             else: promuj_po_opublikowaniu = 0
-            
-            time_truck = int(time.time()) / 750
+
+            time_truck = int(int(time.time()) / 750)
             id_ogloszenia_na_facebook = int(f'{time_truck}{id_ogloszenia}')
 
             zapytanie_sql = '''
