f0d1330d75fc0b5868c2ceb59e09a37c24e4d47a
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-07-02 14:42:28 +0200

    Working now

--- run.py
+++ run.py
@@ -6558,8 +6558,7 @@ def public_on_adresowo():
             extra_opis = ''
             if picked_offer['RodzajZabudowy'] != '':
                 extra_opis += f"Rodzaj Zabudowy:\n{picked_offer['RodzajZabudowy']}\n\n"
-            if picked_offer['Czynsz'] != 0:
-                extra_opis += f"Czynsz:\n{picked_offer['Czynsz']} zł.\n\n"
+            
             if picked_offer['Umeblowanie'] != "":
                 extra_opis += f"Umeblowanie:\n{picked_offer['Umeblowanie']}\n\n"
             if picked_offer['TechBudowy'] != "":
