bfa82dc464bfb01a161d1899d75c54c85721f98b
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-10-19 17:13:29 +0200

    Working now

--- run.py
+++ run.py
@@ -11884,6 +11884,7 @@ def fb_groups_sender():
     section = data.get('section')
     get_id_gallery = data.get('id_gallery')
     created_by = data.get('created_by')
+    msq.handle_error(f"created_by {created_by} category {category}  section {section}, id:{post_id}", log_path=logFileName)
     if get_id_gallery == "None":
         id_gallery = None
     else:
