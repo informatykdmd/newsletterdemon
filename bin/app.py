53c3ef41551e3a0ef9ce4195c6fd8504ca2c16e1
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-11-01 09:54:43 +0100

    Working now

--- bin/app.py
+++ bin/app.py
@@ -154,7 +154,7 @@ def prepare_prompt(began_prompt):
             f"UPDATE Messages SET status = %s WHERE id = %s",
             (1, theme["id"])):
             if dump[1] != "aifa":
-                ready_prompt += f'LOGIN TO:{theme["user_name"]}\nRANGA TO: {theme["description"]}\nWIADOMOŚĆ OD UŻYTKOWNIKA {theme["user_name"]} TO:\n{theme["content"]}\n{command}\n'
+                ready_prompt += f'LOGIN TO: {theme["user_name"]}\nRANGA TO: {theme["description"]}\nWIADOMOŚĆ OD UŻYTKOWNIKA {theme["user_name"]} TO:\n{theme["content"]}\n{command}\n'
                 # ready_prompt += f'LOGIN:{theme["user_name"]}\nRANGA: {theme["description"]}\nINFORMACJE O UŻYTKOWNIKU: {theme["user_about"]}\nWIADOMOŚĆ OD UŻYTKOWNIKA {theme["user_name"]}:\n{theme["content"]}\n{command}\n'
             else:
                 ready_prompt += f'TWÓJ LOGIN TO: aifa\nWIADOMOŚĆ OD CIEBIE TO:\n{theme["content"]}\n\n'
